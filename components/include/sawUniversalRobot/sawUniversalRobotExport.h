3816ded4af2080286296b6a8196e097c2f40bf2e
Peter Kazanzides <pkaz@jhu.edu>
2017-02-22 16:53:49 -0500

    Fix for Windows DLLs

--- components/include/sawUniversalRobot/sawUniversalRobotExport.h
+++ components/include/sawUniversalRobot/sawUniversalRobotExport.h
@@ -14,7 +14,7 @@ http://www.cisst.org/cisst/license.txt.
 */
 
 // check if this module is build as a DLL
-#ifdef sawUniversalRobotController_EXPORTS
+#ifdef sawUniversalRobot_EXPORTS
 #define CISST_THIS_LIBRARY_AS_DLL
 #endif
 
